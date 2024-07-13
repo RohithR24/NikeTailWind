e8be894b7dc345cf34bc44516d38214c3d677c8a
RohithR24 <srohithreddy.edu@gmail.com>
2024-07-12 22:37:19 -0700

    Adding the navbar styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import { NavBar } from "./components/index";
+
 const App = () =>(
  <main className="relative">
-    Nav
+    <NavBar/>
   <section className="xl:padding-1 wide:padding-r padding-b">
     Hero
   </section>
@@ -10,11 +12,20 @@ const App = () =>(
   <section className="padding">
     quality
   </section>
-  <section className="padding">
+  <section className="padding-x py-10">
     Services
   </section>
   <section className="padding">
     Special Offers
+  </section>
+  <section className="padding bg-blue-200">
+    Customer Reviews
+  </section>
+  <section className="padding-x sm:py-32 py-16 w-full">
+    Subscribe
+  </section>
+  <section className="padding-x padding-t pb-8 bg-black">
+    Footer
   </section>
  </main>
 );
