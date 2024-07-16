c37889423d1f9ae8d40ec248b8bd61df54da1396
RohithR24 <srohithreddy.edu@gmail.com>
2024-07-16 00:13:55 -0700

    Adding shoe section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,44 @@
-import React from 'react'
-import {Button, Statistics} from './index'
-import { arrowRight } from '../assets/icons';
-import {statistics} from "../constants/index";
+import React from "react";
+import { Button, Statistics } from "./index";
+import { arrowRight } from "../assets/icons";
+import { bigShoe1 } from "../assets/images";
 
 const Hero = () => {
   return (
-    <section id="home" className= "w-full xl:flex-row flex-col min-h-screen leading-normal font-montserrat justify-center gap-10 max-container border-2 border-red-500 p-2">
-        
-        <div className = " xl:2/5 flex flex-col justify-center items-start max-xl:padding-x pt-28">
-            <p className='pb-8 text-orange-600 text-xl font-montserrat'>
-            Our Summer collections
-            </p>
+    <section
+      id="home"
+      className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
+    >
+      <div className=" xl:2/5 flex flex-col justify-center items-start max-xl:padding-x pt-28">
+        <p className="pb-8 text-orange-600 text-xl font-montserrat">
+          Our Summer collections
+        </p>
 
-            <h1 className='text-8xl font-semibold'>
-            <span>The New Arrival</span> <br/>
-            <span className=' text-orange-600'>Nike</span> Shoes
-            </h1>
+        <h1 className="text-8xl font-semibold">
+          <span>The New Arrival</span> <br />
+          <span className=" text-orange-600">Nike</span> Shoes
+        </h1>
 
-            <p className='pb-10 pt-8 text-xl font-montserrat text-slate-600'>Discover stylish Nike arrivals, quality <br/> comfort, and innovation for your active life.</p>
-            <Button className="" title="Show now" imgURL ={arrowRight}/>
+        <p className="pb-10 pt-8 text-xl font-montserrat text-slate-600">
+          Discover stylish Nike arrivals, quality <br /> comfort, and innovation
+          for your active life.
+        </p>
+        <Button className="" title="Show now" imgURL={arrowRight} />
 
-            <Statistics/>
+        <Statistics />
+        </div>
+
+        <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+          <img
+            src={bigShoe1}
+            alt="shoe"
+            width={610}
+            height={502}
+            className="object-contain relative z-10"
+          />
         </div>
     </section>
-  )
-}
+  );
+};
 
 export default Hero;
