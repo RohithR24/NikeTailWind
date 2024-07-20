a26141a0e5a75c1c02df26959b439baa35e4cec1
RohithR24 <srohithreddy.edu@gmail.com>
2024-07-19 19:50:19 -0500

    Adding products section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
-const Button = ({title, imgURL}) => {
+const Button = ({ title, imgURL }) => {
   return (
-        <button className='flex justify-center border items-center px-10 gap-2 text-white bg-coral-red py-4 font-montserrat leading-normal text-xl rounded-full'>
-          {title}
-          <img className="bg-white rounded-full"src={imgURL} />
-      </button>
-  )
-}
+    <button className="flex justify-center border items-center px-10 gap-2 text-white bg-coral-red py-4 font-montserrat leading-normal text-xl rounded-full">
+      {title}
+      <img className="bg-white rounded-full" src={imgURL} />
+    </button>
+  );
+};
 
-export default Button
+export default Button;
