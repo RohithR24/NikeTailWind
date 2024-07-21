4e0f89e7f574a90c9bc50f8a7164f6a5f20194a3
RohithR24 <srohithreddy.edu@gmail.com>
2024-07-21 13:29:36 -0500

    Styning shoes

--- src/components/PopularProducts.jsx
+++ src/components/PopularProducts.jsx
@@ -12,16 +12,16 @@ const PopularProducts = () => {
         Experience top-notch quality and style with our sought-after <br />{" "}
         selections. Discover a world of comfort, design, and value
       </p>
-      <div className="flex flex-row gap-4 justify-center items-center">
+      <div className="flex flex-row gap-4 justify-center items-center padding-y">
         {products.map((item, index) => (
-          <div className="flex flex-col justify-center items-start" key={index}>
+          <div className="flex flex-col  justify-center leading-relaxed	items-start font-montserrat text-2xl font-medium" key={index}>
             <img src={item.imgURL} alt={"Shoe Image"} />
-            <span className="flex flex-row gap-1 justify-center items-center">
+            <span className="flex flex-row gap-1 justify-center items-center ">
               <img src={star} alt={"rating star"} />
-              {item.rating}
+              ({item.rating})
             </span>
-            <span>{item.name}</span>
-            <span className="text-coral-red font-bold">{item.price}</span>
+            <span className="font-bold">{item.name}</span>
+            <span className="font-bold text-coral-red">{item.price}</span>
           </div>
         ))}
       </div>
