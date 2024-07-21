9438a9beae028705cd03f307b8bb827a857a8cd6
RohithR24 <srohithreddy.edu@gmail.com>
2024-07-21 13:58:23 -0500

    Adding responsove to the popular product section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="ml-11 w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p className="pb-8 text-coral-red text-xl font-montserrat">
--- src/components/PopularProducts.jsx
+++ src/components/PopularProducts.jsx
@@ -8,20 +8,24 @@ const PopularProducts = () => {
       <h2 className="text-4xl font-semibold pb-6">
         Our <span className="text-coral-red"> Popular </span> Products{" "}
       </h2>
-      <p className="text-md font-montserrat text-slate-600">
+      <p className="text-xl text-md font-montserrat text-slate-600">
         Experience top-notch quality and style with our sought-after <br />{" "}
         selections. Discover a world of comfort, design, and value
       </p>
-      <div className="flex flex-row gap-4 justify-center items-center padding-y">
+      <div className="grid gap-6 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-2 padding-y">
         {products.map((item, index) => (
-          <div className="flex flex-col  justify-center leading-relaxed	items-start font-montserrat text-2xl font-medium" key={index}>
+          <div
+            className="flex flex-col justify-center leading-relaxed items-start font-montserrat text-2xl font-medium"
+            key={index}
+          >
             <img src={item.imgURL} alt={"Shoe Image"} />
-            <span className="flex flex-row gap-1 justify-center items-center ">
-              <img src={star} alt={"rating star"} />
-              ({item.rating})
+            <span className="flex flex-row gap-1 justify-center items-center">
+              <img src={star} alt={"rating star"} />({item.rating})
             </span>
-            <span className="font-bold">{item.name}</span>
-            <span className="font-bold text-coral-red">{item.price}</span>
+            <span className="font-bold">
+              {item.name} <br/>
+              <span className="text-coral-red">{item.price}</span>
+            </span>
           </div>
         ))}
       </div>
