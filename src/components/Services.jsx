0dae85d90d7de917df8ead7a59f29a532c5ae9bb
RohithR24 <srohithreddy.edu@gmail.com>
2024-07-30 14:24:48 -0700

    Refactoring the service section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,11 +3,9 @@ import ServiceCard from "../components/ServiceCard";
 
 const Services = () => {
   return (
-    <section className="max-container flex justify-center flex-wrap gap-9">
+    <section className='max-container flex justify-center flex-wrap gap-9'>
       {services.map((service, index) => (
-        <div className="">
           <ServiceCard key={index} {...service} />
-        </div>
       ))}
     </section>
   );
--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -3,10 +3,9 @@ import Button from "./Button";
 const Subscribe = () => {
   return (
     <section className="max-container flex max-lg:flex-col justify-between items-center">
-        <h4 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold mb-5">
-          Sign Up for <span className="text-coral-red">Updates</span> & 
-          Newsletter
-        </h4>
+      <h4 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold mb-5">
+        Sign Up for <span className="text-coral-red">Updates</span> & Newsletter
+      </h4>
 
       <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
         <input type="text" placeholder="subscribe@nike.com" className="input" />
