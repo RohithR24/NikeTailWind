aa4bf228383aa74a4e6bc25b8ea104c244b4b010
RohithR24 <srohithreddy.edu@gmail.com>
2024-07-31 12:09:21 -0700

    Fixing the padding issue for hero

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import {
 const App = () => (
   <main className="relative">
     <NavBar />
-    <section className="xl:padding-1 wide:padding-r padding-b">
+    <section className="xl:padding-1 xl:pl-12 wide:padding-r padding-b">
       <Hero />
     </section>
     <section className="padding">
